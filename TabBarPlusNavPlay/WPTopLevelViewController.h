f6b423a87ac2323b85210fba9ef5a60a95ee784b
Warren Postma <warren.postma@gmail.com>
2013-02-22 11:25:07 -0500

    Added navigation controller
    Added navigation controller.  stackoverflow answer by Ares:
    http://stackoverflow.com/questions/15027055/in-xcode-4-6-how-do-you-crea
    te-a-brand-new-ios-application-combining-a-tabbed-ma#15028245

---
+++ TabBarPlusNavPlay/WPTopLevelViewController.h
@@ -0,0 +1,13 @@
+//
+//  WPTopLevelViewController.h
+//  TabBarPlusNavPlay
+//
+//  Created by Warren Postma on 2013-02-22.
+//  Copyright (c) 2013 Warren Postma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPTopLevelViewController : UIViewController
+
+@end
---
+++ TabBarPlusNavPlay/WPTopLevelViewController.m
@@ -0,0 +1,41 @@
+//
+//  WPTopLevelViewController.m
+//  TabBarPlusNavPlay
+//
+//  Created by Warren Postma on 2013-02-22.
+//  Copyright (c) 2013 Warren Postma. All rights reserved.
+//
+
+#import "WPTopLevelViewController.h"
+
+@interface WPTopLevelViewController ()
+
+@end
+
+@implementation WPTopLevelViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+   
+                self.title = NSLocalizedString(@"Navigate", @"Navigate");
+                self.tabBarItem.image = [UIImage imageNamed:@"navigate"];
+        
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
